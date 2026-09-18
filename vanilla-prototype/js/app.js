4d63c484896588afa956095740b958568adda30e
primedgeslt <primedgesolutions@gmail.com>
2026-09-18 20:07:55 +0100

    Add transaction form handling to app.js

--- vanilla-prototype/js/app.js
+++ vanilla-prototype/js/app.js
@@ -1,1 +1,9 @@
+const transactionForm = document.querySelector("#transaction-form");
 
+let transactions = [];
+
+transactionForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  console.log("The form was submitted.");
+});
