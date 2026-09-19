e9f1a72461a7ff7a80fde9639799069d2c47444a
primedgeslt <primedgesolutions@gmail.com>
2026-09-19 09:11:09 +0100

    Wrap form submission listener in a conditional check

--- vanilla-prototype/js/app.js
+++ vanilla-prototype/js/app.js
@@ -2,8 +2,11 @@ const transactionForm = document.querySelector("#transaction-form");
 
 let transactions = [];
 
-transactionForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+if (transactionForm) {
+  transactionForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  console.log("The form was submitted.");
-});
+    console.log("The form was submitted.");
+  });
+}
+
