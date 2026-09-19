146eab9e5ef692f30bb47077baa84ec533cb550d
primedgeslt <primedgesolutions@gmail.com>
2026-09-19 13:36:01 +0100

    Implement transaction form submission handling
    Added functionality to collect user input and create a transaction object upon form submission.

--- vanilla-prototype/js/app.js
+++ vanilla-prototype/js/app.js
@@ -7,3 +7,30 @@ transactionForm.addEventListener("submit", function (event) {
 
   console.log("The form was submitted.");
 });
+
+
+//collect the user’s input and turn it into one transaction object.
+
+const transactionForm = document.querySelector("#transaction-form");
+
+const descriptionInput = document.querySelector("#description");
+const amountInput = document.querySelector("#amount");
+const typeInput = document.querySelector("#type");
+const categoryInput = document.querySelector("#category");
+const dateInput = document.querySelector("#date");
+
+let transactions = [];
+
+transactionForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const newTransaction = {
+    id: Date.now(),
+    description: descriptionInput.value.trim(),
+    amount: Number(amountInput.value),
+    type: typeInput.value,
+    category: categoryInput.value,
+    date: dateInput.value
+  };
+  console.log(newTransaction);
+});
